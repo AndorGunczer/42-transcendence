f93f144d9abe635157d6bdfb3bd2814122ea1e01
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2024-09-12 13:47:26 +0200

    test friendships for testing and different htmls depending on if the user is logged in or not

--- webapp/transcendence/transcendence/management/commands/populate_avatars.py
+++ webapp/transcendence/transcendence/management/commands/populate_avatars.py
@@ -1,10 +1,7 @@
 # your_app/management/commands/populate_avatars.py
 from django.core.management.base import BaseCommand
-from transcendence.models import Avatar
-import os
-
-from django.core.management.base import BaseCommand
-from transcendence.models import Users2, Avatar
+from transcendence.models import Users2, Avatar, Friends
+from django.db.models import Q
 import os
 
 class Command(BaseCommand):
@@ -18,10 +15,12 @@ class Command(BaseCommand):
 
         # Create users with hashed passwords
         users = [
-            {"username": "test1", "password": "12345", "email": "andor.gunczer@gmail.com", "avatarDirect": "https://127.0.0.1:8000/static/images/anolis_avatar.jpg", "twofa": "True"},
-            {"username": "test2", "password": "12345", "email": "andor.gunczer@gmail.com", "avatarDirect": "https://127.0.0.1:8000/static/images/cat_avatar.jpg", "twofa": "False"},
-            {"username": "test3", "password": "12345", "email": "andor.gunczer@gmail.com", "avatarDirect": "https://127.0.0.1:8000/static/images/dog_avatar.jpg", "twofa": "False"},
+            {"username": "test1", "password": "12345", "email": "andor.gunczer@gmail.com", "avatarDirect": "https://localhost/static/images/anolis_avatar.jpg", "twofa": "True"},
+            {"username": "test2", "password": "12345", "email": "andor.gunczer@gmail.com", "avatarDirect": "https://localhost/static/images/cat_avatar.jpg", "twofa": "False"},
+            {"username": "test3", "password": "12345", "email": "andor.gunczer@gmail.com", "avatarDirect": "https://localhost/static/images/dog_avatar.jpg", "twofa": "False"},
         ]
+
+        users_db = []
 
         for user_data in users:
             user, created = Users2.objects.get_or_create(username=user_data["username"])
@@ -31,8 +30,25 @@ class Command(BaseCommand):
                 user.avatarDirect = user_data["avatarDirect"]
                 user.twofa = user_data["twofa"]
                 user.save()
+                users_db.append(user)
             else:
                 self.stdout.write(self.style.WARNING(f'User {user_data["username"]} already exists'))
+
+        for user in users_db:
+            other_users = Users2.objects.all().exclude(username=user.username)
+
+            for other_user in other_users:
+                try: 
+                    friendship = Friends.objects.get(
+                        Q(friend1=other_user, friend2=user) |
+                        Q(friend1=user, friend2=other_user)
+                    )
+
+                except Friends.DoesNotExist:
+                    new_friendship = Friends(friend1=user, friend2=other_user, state="accepted")
+                    new_friendship.save()
+
+
 
         # Populate Avatar table
         image_files = os.listdir(static_images_dir)
